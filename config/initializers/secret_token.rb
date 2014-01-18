0c82eb46fa4d2491c0d163a41261c57441a0ea8b
Anto Montani <anto@antomontani.com>
2014-01-18 18:55:33 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Askredditalert::Application.config.secret_key_base = 'b24042d690446cef28d700d823ff148cb707d81d97be33e8a777606de23ce5fc18ebd9027122a1d642b2573d8486d9db9313f9cd191608c6dee48d36395cc1fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Askredditalert::Application.config.session_store :cookie_store, key: '_askredditalert_session'
