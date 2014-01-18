92d85529326b8057dc8185ced30c1b934f89e0db
Anto Montani <anto@antomontani.com>
2014-01-18 21:22:11 +0100

    new relic

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-
+	default :from => '"Ask Reddit Alerts" <anto@antomontani.com>'
   def notify_new_thread(alert,url,title)
     @user = alert.user
     @url  = url
