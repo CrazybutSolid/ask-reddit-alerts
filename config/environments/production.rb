d5ce2543f2a767c6832c53c226e90b1a090c7edc
Anto Montani <anto@antomontani.com>
2014-01-18 20:55:12 +0100

    new relic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Askredditalert::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = { :host => 'antomontani.com' }
+  config.action_mailer.default_url_options = { :host => 'askredditalerts.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtpout.secureserver.net',
